91e3f919a8c4d13cf1742083fa62dc02272c257d
hanna <hannamyr.bd@gmail.com>
2021-03-14 08:57:55 +0200

    Home page

---
+++ block/header.php
@@ -0,0 +1,33 @@
+<header>
+<div class="header-block">
+<div class="header-block__logo">
+<a href=""	class="header__logo"><img src="img/1.png">Clay  Shop</a></div>
+	<div class="social-networks-block">
+  <div class="social__wrapper">
+    <a class="social-networks-item" href="" ><img src="img/i.png" alt=""></a>
+		<a class="social-networks-item" href=""><img src="img/tw.png" alt=""></a>
+    <a class="social-networks-item" href="" ><img src="img/f.png" alt=""></a>
+</div></div>
+<div class="searsh__basket">
+<a class="searsh-item" href="" ><img src="img/s.png" alt=""></a>
+<a class="basket-item" href="" ><img src="img/c.png" alt=""></a>
+</div>
+</div>
+  <nav	class="menu">	
+  <ul	class="menu__list">
+  <li><a href=""	class="menu__link">Home</a></li>
+  <li><a href=""	class="menu__link2">Shop<img src="img/arrow_small.png" alt=""></a>  </li>
+  <div class="menulink__cours">
+  <ul>
+  <li><a href=""	class="menu__courseslink">Louis Vouiton</a></li>
+  <li><a href=""	class="menu__courseslink">Gucci</a></li>
+  <li><a href=""	class="menu__courseslink">Valentino</a></li>
+  <li><a href=""	class="menu__courseslink">Givenchy</a></li>
+  </ul>
+  </div>
+
+  <li><a href=""	class="menu__link">Blog</a></li>
+  <li><a href=""	class="menu__link">Contact</a></li>
+</ul>
+</nav>
+</header>
---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link href="css/style.css" rel="stylesheet">
+	<title>Document</title>
+</head>
+<body>
+<div class="container">
+	<?php	require	"block/header.php";	?>
+	<div class="container__form">
+	<form action="#"	method="post"	enctype="multipart/form-data">
+	<div class="container__banner">
+	
+	<div class="container__banner-text">
+		<a href="">Admire Stylish 
+Dresses & Looks</a>
+		<p>If we wanted to build a human-level tool to offer 
+automated outfit advice, we needed to understand 
+people’s fashion taste. </p>
+		<button>Show More</button>
+	</div>
+	</div>
+	<div class="container__form-temp">
+	
+	<div class="container__women-category">
+	<div class="container__women-category-collection"></div>
+	<div class="container__women-category-categ">
+		<div class="container__women-category-categ-gr">
+	<div class="container__women-category-categ-bestsel"></div>
+	<div class="container__women-category-categ-dress"></div></div>
+	<div class="container__women-category-categ-jacket"></div>
+	</div>
+	</div>
+	
+	<div class="container__product-landacape">
+	</div>
+	
+	<div class="containeer__you-may-like">
+	<div class="product__card"></div>
+	<div class="product__card2"></div>
+	<div class="product__card3"></div>
+	</div>
+	
+	</div>
+	</form>
+	</div>
+	<?php	require	"block/footer.php";	?>
+	</div>
+</body>
+</html>
