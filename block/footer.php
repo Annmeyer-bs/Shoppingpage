22a325573b4e599f281d57e3753b83f382652817
hanna <hannamyr.bd@gmail.com>
2021-03-17 00:10:49 +0200

    Shopping page

--- block/footer.php
+++ block/footer.php
@@ -1,25 +1,5 @@
 <footer class="footer">
-	<div class="navfoot">
-    <nav>
-      <ul	class="menu__listfo">
-        <li><a href=""	class="menu__link">Home</a></li>
-        <li><a href=""	class="menu__link">Shop</a></li>
-        <li><a href=""	class="menu__link">Blog</a></li>
-        <li><a href=""	class="menu__link">Contact</a></li>
-      </ul>
-    </nav>
-  </div>
   <div class="container__footer">
-    <div class="menu__info">  
-      <div class="header-block__logo">
-        <img src="img/1.png">Clay  Shop
-      </div>
-      <ul	class="menu__infoes">
-          <li><p  class="inf">Fashion is a popular <br>
-aesthetic expression at a particular time, place and in a specific context, especially in clothing, footwear, lifestyle, accessories, makeup.</p>
-          </li>
-      </ul>
-    </div>
     <div class="menu__inter"><p class="mc__block">Hot links</p>	
       <ul	class="menu__courses">
         <li><a href=""	class="menu__courseslink">Home</a></li>
@@ -41,6 +21,14 @@ aesthetic expression at a particular time, place and in a specific context, espe
         <li><a href=""	class="menu__courseslink">FAQ</a></li>
         <li><a href=""	class="menu__courseslink">Terms of use</a></li>
         <li><a href=""	class="menu__courseslink">Privacy Policy</a></li>
+      </ul>
+    </div>
+    <div class="menu__inter"><p class="mc__block">Get newsletter</p>	
+      <ul	class="menu__email">
+        <li><a href=""	class="menu__courseslink">Get updates about style</a></li>
+        <li class="inp"><input type="email"  placeholder="Type your email…">
+        <button></button>
+        </li>
       </ul>
     </div>
   </div>
--- js/script.js
+++ js/script.js
@@ -28,8 +28,35 @@ new	Swiper('.swiper-container',{
 $(document).ready(function(){
 	$('.slideyml').slick({
 		dots:true,
-		slidesToShow: 1,
+		infinite: true,
+		slidesToShow: 3,
 		autoplay: true,
 		autoplaySpeed: 2000,
+		adaptiveHeight: true,
+		responsive: [
+			{
+			  breakpoint: 1024,
+			  settings: {
+				slidesToShow: 3,
+				slidesToScroll: 3,
+				infinite: true,
+				dots: true
+			  }
+			},
+			{
+			  breakpoint: 600,
+			  settings: {
+				slidesToShow: 2,
+				slidesToScroll: 2
+			  }
+			},
+			{
+			  breakpoint: 480,
+			  settings: {
+				slidesToShow: 1,
+				slidesToScroll: 1
+			  }
+			}
+		  ]
 	});
   });
